c2414d63f2a0ecba604566985da052278eb19785
Gianluca Guarini <gianluca.guarini@gmail.com>
2019-06-19 23:08:34 +0200

    updated: dev dependencies

--- dist/umd.dom-bindings.js
+++ dist/umd.dom-bindings.js
@@ -1554,64 +1554,6 @@
     }
   }
 
-  /**
-   * Method used to bind expressions to a DOM node
-   * @param   {string|HTMLElement} html - your static template html structure
-   * @param   {Array} bindings - list of the expressions to bind to update the markup
-   * @returns {TemplateChunk} a new TemplateChunk object having the `update`,`mount`, `unmount` and `clone` methods
-   *
-   * @example
-   *
-   * riotDOMBindings
-   *  .template(
-   *   `<div expr0><!----></div><div><p expr1><!----><section expr2></section></p>`,
-   *   [
-   *     {
-   *       selector: '[expr0]',
-   *       redundantAttribute: 'expr0',
-   *       expressions: [
-   *         {
-   *           type: expressionTypes.TEXT,
-   *           childNodeIndex: 0,
-   *           evaluate(scope) {
-   *             return scope.time;
-   *           },
-   *         },
-   *       ],
-   *     },
-   *     {
-   *       selector: '[expr1]',
-   *       redundantAttribute: 'expr1',
-   *       expressions: [
-   *         {
-   *           type: expressionTypes.TEXT,
-   *           childNodeIndex: 0,
-   *           evaluate(scope) {
-   *             return scope.name;
-   *           },
-   *         },
-   *         {
-   *           type: 'attribute',
-   *           name: 'style',
-   *           evaluate(scope) {
-   *             return scope.style;
-   *           },
-   *         },
-   *       ],
-   *     },
-   *     {
-   *       selector: '[expr2]',
-   *       redundantAttribute: 'expr2',
-   *       type: bindingTypes.IF,
-   *       evaluate(scope) {
-   *         return scope.isVisible;
-   *       },
-   *       template: riotDOMBindings.template('hello there'),
-   *     },
-   *   ]
-   * )
-   */
-
   exports.bindingTypes = bindingTypes;
   exports.createBinding = create$5;
   exports.createExpression = create$2;
