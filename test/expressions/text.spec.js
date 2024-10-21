a2042b891a0a8369d4ae0de0a7e70ba27bf123e7
Maksim Kachurin <kachurun@gmail.com>
2024-10-20 22:39:09 -0700

    Add tests for text expression

--- test/expressions/text.spec.js
+++ test/expressions/text.spec.js
@@ -81,4 +81,34 @@ describe('text specs', () => {
 
     expect(p.textContent).to.be.equal('0')
   })
+
+  it('Text can restore value upon update (https://github.com/riot/riot/issues/3023)', () => {
+    const target = document.createElement('div')
+    const el = template('<p expr0><----></p>', [
+      {
+        selector: '[expr0]',
+        expressions: [
+          {
+            type: expressionTypes.TEXT,
+            childNodeIndex: 0,
+            evaluate: (scope) => scope.val,
+          },
+        ],
+      },
+    ]).mount(target, { val: 'hello' })
+
+    const p = target.querySelector('p')
+
+    // normal update
+    expect(p.textContent).to.be.equal('hello')
+    el.update({ val: 'world' })
+
+    expect(p.textContent).to.be.equal('world')
+
+    p.childNodes[0].data = 'Boo!'
+
+    // update to same value but after value was changed from the outside
+    el.update({ val: 'world' })
+    expect(p.textContent).to.be.equal('world')
+  })
 })
